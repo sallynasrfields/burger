bbfd16cd3e049498f91db32d7f199790a68fb9fa
Sally N Fields <sally.nasr@gmail.com>
2017-06-11 23:20:30 -0500

    connections issues

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,17 @@
 var mysql = require("mysql");
-var connection;
 
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }
-// else {
-    connection = mysql.createConnection({
-        port:3060 || process.env.PORT,
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    var connection = mysql.createConnection({
+        port: 3306,
         host: "localhost",
         user: "root",
         password: "",
         database: "burgers_db"
     });
-// }
+};
 
 connection.connect(function (err) {
     if (err) {
