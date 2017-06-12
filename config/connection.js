8186e1de3d286644302bb88e4016227bc8549280
Sally N Fields <sally.nasr@gmail.com>
2017-06-11 23:19:02 -0500

    change connection details

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// }
+// else {
     connection = mysql.createConnection({
+        port:3060 || process.env.PORT,
         host: "localhost",
         user: "root",
         password: "",
         database: "burgers_db"
     });
-}
+// }
 
 connection.connect(function (err) {
     if (err) {
